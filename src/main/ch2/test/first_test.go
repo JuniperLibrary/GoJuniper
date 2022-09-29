41336a9fb5ffc6278a46fa0982cf07f2344bf1f7
wanglufei <2686221966@qq.com>
2022-09-29 16:51:41 +0800

    第一个测试程序

---
+++ src/main/ch2/test/first_test.go
@@ -0,0 +1,12 @@
+/**
+ 编写单元测试程序
+1. 源码文件以 _test结尾：xxx_test.go
+2. 测试方法以Test开头： func TestFirstTry(t *testing.T) {...}
+*/
+package try_test
+
+import "testing"
+
+func TestFirstTry(t *testing.T) {
+	t.Log("My first try!")
+}
